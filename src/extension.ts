f6808c0b669fec28748e5864af7dc67aba85ff99
Michal Zielenkiewicz <michal@oasisprotocol.org>
2022-08-02 22:27:22 +0200

    Handle new extension, add licence, and basic unit test

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,40 @@
-import * as vscode from 'vscode';
+import { commands, window, ExtensionContext, Uri } from 'vscode';
 
-export function activate({ subscriptions }: vscode.ExtensionContext) {
+export function activate({ subscriptions }: ExtensionContext) {
   let NEXT_TERM_ID = 1;
 
-  const singleWatch = vscode.commands.registerCommand('jestcontextmenurunner.singleWatch', (uri: vscode.Uri) => {
-    vscode.window.showInformationMessage(`Running tests in watch mode for ${getFileName(uri)} file.`);
-    executeCommandInTerminal(NEXT_TERM_ID++, `--watch ${uri.fsPath}`);
-  });
+  const singleWatch = commands.registerCommand(
+    'jestcontextmenurunner.singleWatch',
+    (uri: Uri) => {
+      window.showInformationMessage(
+        `Running tests in watch mode for ${getFileName(uri)} file.`
+      );
+      executeCommandInTerminal(NEXT_TERM_ID++, `--watch ${uri.fsPath}`);
+    }
+  );
 
-  const single = vscode.commands.registerCommand('jestcontextmenurunner.single', (uri: vscode.Uri) => {
-    vscode.window.showInformationMessage(`Running tests for ${getFileName(uri)} file.`);
-    executeCommandInTerminal(NEXT_TERM_ID++, uri.fsPath);
-  });
+  const single = commands.registerCommand(
+    'jestcontextmenurunner.single',
+    (uri: Uri) => {
+      window.showInformationMessage(
+        `Running tests for ${getFileName(uri)} file.`
+      );
+      executeCommandInTerminal(NEXT_TERM_ID++, uri.fsPath);
+    }
+  );
 
-  const all = vscode.commands.registerCommand('jestcontextmenurunner.all', () => {
-    vscode.window.showInformationMessage('Running all tests.');
+  const all = commands.registerCommand('jestcontextmenurunner.all', () => {
+    window.showInformationMessage('Running all tests.');
     executeCommandInTerminal(NEXT_TERM_ID++);
   });
 
-  const allWatch = vscode.commands.registerCommand('jestcontextmenurunner.allWatch', () => {
-    vscode.window.showInformationMessage('Running all tests in watch mode.');
-    executeCommandInTerminal(NEXT_TERM_ID++, '--watch');
-  });
+  const allWatch = commands.registerCommand(
+    'jestcontextmenurunner.allWatch',
+    () => {
+      window.showInformationMessage('Running all tests in watch mode.');
+      executeCommandInTerminal(NEXT_TERM_ID++, '--watch');
+    }
+  );
 
   subscriptions.push(singleWatch);
   subscriptions.push(single);
@@ -29,13 +42,15 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
   subscriptions.push(allWatch);
 }
 
-function getFileName(uri: vscode.Uri): string {
-  const searchResult = /^.+(\\|\/)(?<fileName>([^\\\/\n]+)(\.)?[^\n\.]+)$/.exec(uri.fsPath);
+function getFileName(uri: Uri): string {
+  const searchResult = /^.+(\\|\/)(?<fileName>([^\\\/\n]+)(\.)?[^\n\.]+)$/.exec(
+    uri.fsPath
+  );
   return searchResult?.groups?.fileName || '';
 }
 
 function executeCommandInTerminal(id: number, cmd = ''): void {
-  const terminal = vscode.window.createTerminal(`Ext Terminal #${id}`);
+  const terminal = window.createTerminal(`Ext Terminal #${id}`);
   terminal.show(true);
   terminal.sendText(`npm run env -- jest --color ${cmd}`);
 }
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,17 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  test('Registered commands', async () => {
+    const extention = vscode.extensions.getExtension(
+      'modifiz.jestcontextmenurunner'
+    )!;
+    await extention.activate();
+    const commands = await vscode.commands.getCommands();
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    assert.ok(commands.includes('jestcontextmenurunner.singleWatch'));
+    assert.ok(commands.includes('jestcontextmenurunner.single'));
+    assert.ok(commands.includes('jestcontextmenurunner.allWatch'));
+    assert.ok(commands.includes('jestcontextmenurunner.all'));
+  });
 });
