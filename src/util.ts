9c0a10969667c1637b5552584caecceacdc9a775
Walter Corrales <corraleswalter@live.com>
2022-10-19 13:22:15 -0600

    [🔨] unused code

--- src/util.ts
+++ src/util.ts
@@ -10,34 +10,4 @@ export function escapeRegExpForPath(s: string): string {
     return s.replace(/[*+?^${}<>()|[\]]/g, '\\$&'); // $& means the whole matched string
 }
 
-export function findFullTestName(selectedLine: number, children: any[]): string | undefined {
-    if (!children) {
-        return;
-    }
-    for (const element of children) {
-        if (element.type === 'describe' && selectedLine === element.start.line) {
-            return resolveTestNameStringInterpolation(element.name);
-        }
-        if (element.type !== 'describe' && selectedLine >= element.start.line && selectedLine <= element.end.line) {
-            return resolveTestNameStringInterpolation(element.name);
-        }
-    }
-    for (const element of children) {
-        const result = findFullTestName(selectedLine, element.children);
-        if (result) {
-            return resolveTestNameStringInterpolation(element.name) + ' ' + result;
-        }
-    }
-}
-
-export function resolveTestNameStringInterpolation(s: string): string {
-    const variableRegex = /(\${?[A-Za-z0-9_]+}?|%[psdifjo#%])/gi;
-    const matchAny = '(.*?)';
-    return s.replace(variableRegex, matchAny);
-}
-
-export function escapeSingleQuotes(s: string): string {
-    return isWindows() ? s : s.replace(/'/g, "'\\''");
-}
-
 export const quote = (s: string) => `"${s}"`;
